12b708bdbb4e661d7cf51ebc6b7b479478a19a9d
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-09-27 15:48:30 +0300

    feat(lsp): add rails server

--- .config/nvim/lua/tima/lsp/configs.lua
+++ .config/nvim/lua/tima/lsp/configs.lua
@@ -3,9 +3,9 @@ if not status_ok then
 	return
 end
 
-local lspconfig = require("lspconfig")
+local lspconfig = require('lspconfig')
 
-local servers = { "jsonls", "sumneko_lua" }
+local servers = { 'jsonls', 'sumneko_lua', 'solargraph' }
 
 lsp_installer.setup({
 	ensure_installed = servers,
@@ -13,12 +13,12 @@ lsp_installer.setup({
 
 for _, server in pairs(servers) do
 	local opts = {
-		on_attach = require("tima.lsp.handlers").on_attach,
-		capabilities = require("tima.lsp.handlers").capabilities,
+		on_attach = require('tima.lsp.handlers').on_attach,
+		capabilities = require('tima.lsp.handlers').capabilities,
 	}
-	local has_custom_opts, server_custom_opts = pcall(require, "tima.lsp.settings." .. server)
+	local has_custom_opts, server_custom_opts = pcall(require, 'tima.lsp.settings.' .. server)
 	if has_custom_opts then
-		opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
+		opts = vim.tbl_deep_extend('force', opts, server_custom_opts)
 	end
 	lspconfig[server].setup(opts)
 end
