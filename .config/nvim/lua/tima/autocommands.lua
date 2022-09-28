75e9c70056c9d6390dec108d668b74773bd8b613
timamuravyov <timamuravyov@timamuravyovs-MacBook-Pro.local>
2022-09-28 09:17:13 +0300

    feat(autocmd): comment visual select on yank

--- .config/nvim/lua/tima/autocommands.lua
+++ .config/nvim/lua/tima/autocommands.lua
@@ -2,7 +2,6 @@ vim.cmd [[
   augroup _general_settings
     autocmd!
     autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
     autocmd InsertEnter * norm zz
@@ -25,3 +24,7 @@ vim.cmd [[
     autocmd FileType markdown setlocal spell
   augroup end
 ]]
+
+-- visual select every time you yank
+--     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
+
