5d2bf66d68703d9f06f9f4f5d2f973b0e0dd1a4f
tim <tima.muravyov@gmail.com>
2023-01-11 01:16:03 +0200

    feat(gitsigns): finalize setup

--- .config/nvim/lua/tima/gitsigns.lua
+++ .config/nvim/lua/tima/gitsigns.lua
@@ -1,8 +1,22 @@
--- import gitsigns plugin safely
-local setup, gitsigns = pcall(require, "gitsigns")
-if not setup then
+local status_ok, gitsigns = pcall(require, "gitsigns")
+if not status_ok then
   return
 end
 
--- configure/enable gitsigns
-gitsigns.setup()
+gitsigns.setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = "single",
+    style = "minimal",
+    relative = "cursor",
+    row = 0,
+    col = 1,
+  },
+}
